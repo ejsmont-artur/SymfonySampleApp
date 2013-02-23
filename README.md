47ba336d5549d59c5bc00e9972da694c5bb666d2
aejsmont <ejsmont.artur@gmail.com>
2013-02-23 21:45:35 +1100

    initial demo project code

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ symfony-php-proxy-builder-example
 =================================
 
 Example Symfony2 application using php-proxy-builder
+
+How this project was created:
+<pre>
+    curl -s http://getcomposer.org/installer | php
+    chmod a+x composer.phar
+    ./composer.phar create-project symfony/framework-standard-edition symfony-php-proxy-builder-example 2.1.6
+</pre>
+
+Then i have updated composer.json to include namespace and removed some Acme files to reduce distractions.
+
+Added two sections to composer.json
+    "psr-0": { "Acme": "src/" }
+    "ejsmont-artur/php-circuit-breaker": "0.0.1"
+
+Updated dependencies:
+    curl -s http://getcomposer.org/installer | php
+    php app/check.php
+    php app/console cache:clear
+    php composer.phar update
+
+phpunit -c app src/Acme/DemoBundle
+
---
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DemoController extends Controller {
+
+    /**
+     * @Route("/", name="_demo")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/hello/{name}", name="_demo_hello")
+     * @Template()
+     */
+    public function helloAction($name) {
+        // get instance from services.xml
+        $circuitBreaker = $this->get('circuitBreaker');
+        
+        // get instance directly from the factory
+        //$factory = new \Ejsmont\CircuitBreaker\Factory();
+        //$circuitBreaker = $factory->getSingleApcInstance(30, 300);
+        
+        if( $circuitBreaker->isAvailable("UserProfileService") ){
+            $name .= ' circuit breaker says its ok';
+        }
+        
+        return array('name' => $name);
+    }
+
+}
